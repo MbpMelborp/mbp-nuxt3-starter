503a82d2936d87f5e616c65f118af273077e3038
Melborpco <accounts@melborp.co>
2022-08-17 11:16:30 -0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<script setup >
+definePageMeta({})
+useHead({
+  title: 'Nuxt3 Eslint',
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="port">imagen</div>
+    <div class="form">form</div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.wrapper {
+  @apply w-full flex;
+  .port {
+    @apply md:w-1/2 w-full;
+  }
+  .form {
+    @apply md:w-1/2 w-full;
+  }
+}
+</style>
+
+
+
+
+
+
